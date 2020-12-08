68276a2cb4106476cb57602023335a1a6b15f747
carlmon <450101+carlmon@users.noreply.github.com>
2020-12-08 11:43:01 +0200

    Use new version of Hashcat and add some wordlists

--- azure-entrypoint.sh
+++ azure-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+DEBIAN_FRONTEND=noninteractive
 
 # Install required packages
-apt-get update 2>error
-apt-get install -y build-essential 2>error # Not installing 'ubuntu-desktop'
+apt-get update
+apt-get install -y apt-transport-https build-essential
 
 # Blacklist nouveau drivers
 cat <<EOT >> /etc/modprobe.d/nouveau.conf
@@ -10,18 +11,31 @@ blacklist nouveau
 blacklist lbm-nouveau
 EOT
 
-# Set up GRID drivers
-wget -O NVIDIA-Linux-x86_64-grid.run https://go.microsoft.com/fwlink/?linkid=874272
-chmod +x NVIDIA-Linux-x86_64-grid.run
-./NVIDIA-Linux-x86_64-grid.run -s>error
-cp /etc/nvidia/gridd.conf.template /etc/nvidia/gridd.conf
-sed -e "\$aIgnoreSP=FALSE" /etc/nvidia/gridd.conf
+# Set up CUDA drivers: http://developer.download.nvidia.com/compute/cuda/repos/
+CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
+wget --quiet -O /tmp/${CUDA_REPO_PKG} https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG}
+dpkg -i /tmp/${CUDA_REPO_PKG}
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+rm -f /tmp/${CUDA_REPO_PKG}
+apt-get update
+apt-get install -y cuda-drivers nvidia-cuda-toolkit
 
 # Install Hashcat
-wget https://hashcat.net/files/hashcat-5.1.0.tar.gz
-tar -xf hashcat-5.1.0.tar.gz
-cd hashcat-5.1.0
+HASHCAT_SRC_PKG=hashcat-6.1.1
+wget https://hashcat.net/files/${HASHCAT_SRC_PKG}.tar.gz
+tar -xf ${HASHCAT_SRC_PKG}.tar.gz
+cd ${HASHCAT_SRC_PKG}
 make && make install
 
 # Install John the Ripper
 snap install john-the-ripper
+
+# Install some wordlists
+mkdir /opt/wordlists
+cd /opt/wordlists
+wget --quiet https://crackstation.net/files/crackstation-human-only.txt.gz && gunzip crackstation-human-only.txt.gz
+# wget --quiet https://crackstation.net/files/crackstation.txt.gz && gunzip -d crackstation.txt.gz
+wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/darkweb2017-top1000.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/darkweb2017-top10000.txt
+wget https://github.com/FlameOfIgnis/Pwdb-Public/raw/master/wordlists/ignis-10M.txt
